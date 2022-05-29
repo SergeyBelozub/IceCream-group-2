5ff22ac4976e11d4dbf09c96c0f2ca49c1be9e92
SergeyBelozub <99657458+SergeyBelozub@users.noreply.github.com>
2022-05-29 13:04:23 +0000

    Deploying to gh-pages from @ SergeyBelozub/IceCream-group-2@8e56136060ccd1a89ce03593ff2a6f3ecb179b52 🚀

---
+++ index.14c759ea.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector(".menu-open-btn"),closeMenuBtn:document.querySelector(".menu-close-btn"),menu:document.querySelector(".mob-menu"),body:document.querySelector(".body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.14c759ea.js.map

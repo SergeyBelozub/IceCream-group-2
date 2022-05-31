3432b9b12224f915a3990a8187fc5877fa546db0
SergeyBelozub <99657458+SergeyBelozub@users.noreply.github.com>
2022-05-31 20:25:30 +0000

    Deploying to gh-pages from @ SergeyBelozub/IceCream-group-2@e60520084e4672a74c7cb0c356ec3d92252eba10 🚀

--- index.c09a03ca.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),openModalBtn2:document.querySelector("[data-modal2-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.querySelector(".body")};function o(){e.body.classList.toggle("no-scroll"),e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openModalBtn2.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.c09a03ca.js.map
